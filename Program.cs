77d6f1c4962c0053a5de69bf17c59f47e5aec4ab
Йоанна Владимирова Симеонова (Повтаря) <YVSimeonova19@codingburgas.bg>
2024-10-14 11:18:09 +0300

    Add cs code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 16, 17, 15, 0, 20, 1, 1, 2 };
+        int n = arr.Length;
+        int maxLoad = 0, load = 0, diff = 0;
+        load = arr.Sum();
+        
+        if (load % n != 0)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+        
+        load /= n;
+
+        for (int i = 0; i < n; i++)
+        {
+            diff += (arr[i] - load);
+
+            if (diff < 0)
+                maxLoad = Math.Max(maxLoad, -diff);
+            else
+                maxLoad = Math.Max(maxLoad, diff);
+        }
+        
+        Console.WriteLine(maxLoad);
+    }
+}
